c392f42e6268ec1be80a006e88eedaa216390ddd
Johasit0 <johanmunoz319@gmail.com>
2024-12-10 13:43:39 -0500

    test de conexion

--- models/index.js
+++ models/index.js
@@ -37,6 +37,13 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
